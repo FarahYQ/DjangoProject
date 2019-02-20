d1a0dd29e22195b051edef2a23e9f1f17c74a306
Farah Quader <fyquader@gmail.com>
2019-02-19 23:51:07 -0800

    Create model for todo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py shell
+# from todo.models import TodoItem
+# TodoItem.objects.all()
+# a = TodoItem(title="Organize", content="Put away books and clothes")
+# a.save()
+# TodoItem.objects.get(id=1) --> creates a query for object with id 1
+
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import TodoItem
 # Create your views here.
 def todoView(request):
-    return render(request, 'todo.html')
-    # return HttpResponse('lots to do')
+    all_items = TodoItem.objects.all()
+    return render(request, 'todo.html', {'all_items': all_items})
